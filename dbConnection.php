a470235541d0bd413a8be441bb4f22b8b53fd9fb
fauziriizkii <114478450+fauziriizkii@users.noreply.github.com>
2023-12-13 20:25:47 +0700

    Add files via upload

---
+++ dbConnection.php
@@ -0,0 +1,8 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'latihan_crud';
+$databaseUsername = 'root';
+$databasePassword = 'root';
+
+// Open a new connection to the MySQL server
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
